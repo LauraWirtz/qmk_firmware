14926f3dfafc6c9ab63766db79fac45928c73160
LauraWirtz <lw.laura.wirtz@gmail.com>
2024-07-28 13:39:53 +0200

    fixed split keeb led indicators

--- keyboards/keebio/nyquist/keymaps/Laura/keymap.c
+++ keyboards/keebio/nyquist/keymaps/Laura/keymap.c
@@ -24,7 +24,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	KC_LBRC,			KC_W, 				KC_F,				KC_M,				KC_P,				KC_V,						KC_COMM,			KC_DOT,				KC_Q,				KC_QUOT,			KC_EQL,				KC_Z,
 	KC_ESC,				LSFT_T(KC_R),		LCTL_T(KC_S),		LGUI_T(KC_N),		LALT_T(KC_T),		KC_B,						KC_MINS,			RALT_T(KC_A),		RGUI_T(KC_E),		RCTL_T(KC_I),		RSFT_T(KC_H),		KC_J,
 	KC_RBRC,			KC_X,				KC_C,				KC_L,				KC_D,				KC_G,						KC_SLSH,			KC_U,				KC_O,				KC_Y,				KC_K,				_______,
-	_______,			TG(_QWERTY),		KC_LSFT,			_______,			LT(_FKEYS, KC_TAB),	LT(_NUM, KC_SPC),			LT(_SYM, KC_ENT),	KC_SCLN,			_______,			_______,			_______,			_______
+	_______,			TG(_QWERTY),		KC_LSFT,			_______,			LT(_FKEYS, KC_TAB),	LT(_NUM, KC_SPC),			LT(_SYM, KC_ENT),	KC_SCLN,			KC_LEFT,			KC_DOWN,			KC_UP,				KC_RIGHT
 	),
   [_FKEYS] = LAYOUT_ortho_5x12(
 	_______,			_______,			_______,			_______,			_______,			_______,					_______,			KC_F10,				KC_F11,				KC_F12,				_______,			_______,
@@ -45,7 +45,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	_______,			_______,			KC_BSPC,			KC_UP,				KC_DEL,				_______,					_______,			KC_7,				KC_8,				KC_9,				KC_0,				_______,
 	_______,			_______,			KC_LEFT,			KC_DOWN,			KC_RIGHT,			_______,					_______,			RALT_T(KC_4),		RGUI_T(KC_5),		RCTL_T(KC_6),		_______,			_______,
 	_______,			_______,			_______,			_______,			_______,			_______,					_______,			KC_1,				KC_2,				KC_3,				_______,			_______,
-	_______,			_______,			_______,			_______,			_______,			_______,					LT(_SYM, KC_0),		_______,			_______,			_______,			_______,			_______
+	_______,			_______,			_______,			_______,			_______,			_______,					LT(_SYM, KC_0),		_______,			KC_HOME,			KC_PGDN,			KC_PGUP,			KC_END
 	),
   [_SYM] = LAYOUT_ortho_5x12(
 	_______,			_______,			_______,			_______,			_______,			_______,					_______,			_______,			_______,			_______,			_______,			_______,
@@ -57,16 +57,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 };
 
 bool rgb_matrix_indicators_user(void) {
-	rgb_matrix_mode(RGB_MATRIX_CUSTOM_pink_gradient);
-
-/*	rgb_matrix_set_color(36, 0x50, 0x08, 0x05);
-	rgb_matrix_set_color(37, 0x50, 0x08, 0x05);
-	rgb_matrix_set_color(38, 0x50, 0x08, 0x05);
-	rgb_matrix_set_color(52,0x50, 0x08, 0x05);
-
-	for (uint8_t i = 0; i < 72; i++) {
-		rgb_matrix_set_color(i, 3*i, 0x00, 0x00);
+	if(IS_LAYER_ON(_QWERTY)) {
+		for (uint8_t i = 0; i < 72; i++) {
+			if (i != 11 && i != 16 && i != 17 && i != 18) {
+				rgb_matrix_set_color(i, 0x00, 0x00, 0x00);
+			}
+		}
 	}
-*/
 	return true;
 };
